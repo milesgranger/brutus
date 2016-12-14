14bfb88ef66053c83242e683e863bb00f14ac7b4
Miles Granger <miles59923@gmail.com>
2016-12-14 13:26:07 +0100

    Fixing wait()

--- brutus/distribute.py
+++ brutus/distribute.py
@@ -4,26 +4,6 @@ import random
 import time
 
 
-def wait(job_connections):
-    """wait for job connections, and return actual result"""
-    single_val = False
-
-    if type(job_connections) == map:
-        job_connections = [r for r in job_connections]
-
-    elif type(job_connections) != list:
-        single_val = True
-        job_connections = [job_connections]
-
-    while True:
-        if all([job.status == 'complete' for job in job_connections]):
-            if single_val:
-                return job_connections[0].result
-            return [job.result for job in job_connections]
-        else:
-            time.sleep(0.1)
-
-
 class JobConnection(object):
     """
     Instance with a connection specific to a job_id
@@ -64,6 +44,50 @@ class JobConnection(object):
         return self._exception
 
 
+
+def wait(job_connections):
+    """
+    Wait for job connections, and return actual result
+    expects job_connections (list, mapping or single instance) to be of instances of JobConnection
+    """
+
+    # TODO: Add check/warning if results have an exception result from JobConnection
+
+    # Determine if we should return a single result, not a lot of results.
+    single_val = False
+
+    # Convert mapping to list
+    if type(job_connections) == map:
+        job_connections = [r for r in job_connections]
+
+    # TODO: Add ability to check dict/kwargs with vals as JobConnection instances
+    elif type(job_connections) == dict:
+        pass
+
+    # If we made it here, and it is not a list, it must be a single value
+    elif type(job_connections) != list:
+        single_val = True
+        job_connections = [job_connections]
+
+    # Test if these are not JobConnection objects
+    if all(['JobConnection' not in str(type(job)) for job in job_connections]):
+        if single_val:
+            return job_connections[0]
+        return job_connections
+
+    # Wait for all JobConnection.status == 'complete' and then get their
+    # result from the scheduler; returning the actual values, as if the
+    # function wasn't @distribute decorated.
+    while True:
+        if all([job.status == 'complete' for job in job_connections]):
+            if single_val:
+                return job_connections[0].result
+            return [job.result for job in job_connections]
+        else:
+            time.sleep(0.1)
+
+
+
 def distribute(func=None, address='localhost:4541'):
     """
     Wrapper which sends function and args/kwargs to given scheduler
@@ -84,9 +108,11 @@ def distribute(func=None, address='localhost:4541'):
 
         def __call__(self, *args, **kwargs):
 
-            # Check all args are not JobConnections, wait if so
-            args = [arg if 'JobConnection' not in str(type(arg)) else wait(arg) for arg in args]
-            kwargs = {kw: arg if 'JobConnection' not in str(type(arg)) else wait(arg) for kw, arg in kwargs.items()}
+            # Wait for all args to come back (assuming they are JobConnection instances)
+            # if they aren't wait() will return the value right away.
+            # TODO: Make this a future, so it doesn't get hung up on a single result/arg taking a while..:
+            args = [wait(arg) for arg in args]
+            kwargs = {kw: wait(arg) for kw, arg in kwargs.items()}
 
             job = dict(job_id='id-{}'.format(random.random()).replace('.', ''),
                        func=self.func,
@@ -103,6 +129,9 @@ def distribute(func=None, address='localhost:4541'):
         return Distribute
 
 
+############################
+###### TESTING #############
+############################
 if __name__ == '__main__':
 
     @distribute
@@ -115,7 +144,12 @@ if __name__ == '__main__':
         time.sleep(random.random())
         return x * 2
 
-    results = map(adder, range(5))
-    results = map(times_2, results)
+    @distribute
+    def add(x):
+        return sum(x)
 
-    print('Results: ', results)
+    results = map(adder, [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    results = map(times_2, results)
+    total = wait(add(results))
+
+    print('Results: ', total)
